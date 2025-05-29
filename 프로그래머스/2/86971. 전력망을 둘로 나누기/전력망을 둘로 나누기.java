65b24638c34f083ef1d6fc4a02a450850441db4e
Dong Geon Na <87890694+doongeon@users.noreply.github.com>
2025-05-29 23:25:45 +0900

    [level 2] Title: 전력망을 둘로 나누기, Time: 0.38 ms, Memory: 87.3 MB -BaekjoonHub

--- 프로그래머스/2/86971. 전력망을 둘로 나누기/전력망을 둘로 나누기.java
+++ 프로그래머스/2/86971. 전력망을 둘로 나누기/전력망을 둘로 나누기.java
@@ -1,38 +1,43 @@
 import java.util.*;
 
 class Solution {
+    int minDiff = 10000000;
+    int N = 0;
+    
     public int solution(int n, int[][] wires) {
         List<Integer>[] adj = new ArrayList[n + 1];
-        for(int i = 0; i <= n; i++) adj[i] = new ArrayList<>();
-        for(int[] w : wires) {
-            int from = w[0];
-            int to = w[1];
+        boolean[] v = new boolean[n + 1];
+        N = n;
+        
+        for(int i = 0; i <= n; i++) {
+            adj[i] = new ArrayList<>();
+        }
+        
+        for(int[] wire : wires) {
+            int from = wire[0];
+            int to = wire[1];
+            
             adj[from].add(to);
             adj[to].add(from);
         }
         
-        int[] c = new int[n + 1];
-        boolean[] v = new boolean[n + 1];
-        dfs(1, adj, v, c);
+        dfs(1, adj, v);
         
-        int answer = 9999;
-        for(int i = 0; i <= n; i++) {
-            answer = Math.min(answer, Math.abs(n - 2 * c[i]));
-            
-        }
-        return answer;
+        return minDiff;
     }
     
-    private int dfs(int cur, List<Integer>[] adj, boolean[] v, int[] result) {
+    private int dfs(int node, List<Integer>[] adj, boolean[] v) {
+        v[node] = true;
         int count = 1;
-        v[cur] = true;
         
-        for(int next : adj[cur]) {
+        for(int next : adj[node]) {
             if(v[next]) continue;
-            count += dfs(next, adj, v, result);
+            count += dfs(next, adj, v);
         }
         
-        result[cur] = count;
+        int tempDiff = Math.abs(N - count * 2);
+        minDiff = Math.min(tempDiff, minDiff);
+        
         return count;
     }
 }
