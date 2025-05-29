fa84963f3a71baa33ddb1609a2ab4f3c47041895
Dong Geon Na <87890694+doongeon@users.noreply.github.com>
2025-05-29 23:55:55 +0900

    [level 1] Title: 신규 아이디 추천, Time: 15.84 ms, Memory: 73.7 MB -BaekjoonHub

--- 프로그래머스/1/72410. 신규 아이디 추천/신규 아이디 추천.java
+++ 프로그래머스/1/72410. 신규 아이디 추천/신규 아이디 추천.java
@@ -1,50 +1,51 @@
 class Solution {
     public String solution(String new_id) {
-        String answer = "";
-        String myId = new_id.toLowerCase();
-        String temp = "";
-        for(int i = 0; i < myId.length(); i++) {
-            char cur = myId.charAt(i);
-            if(cur >= 'a' && cur <= 'z') {
-                temp += myId.charAt(i);
+        String str = new_id;
+        
+        str = str.toLowerCase();
+        String tempStr = "";
+        for(int i = 0; i < str.length(); i++) {
+            if(str.charAt(i) >= 'a' && str.charAt(i) <= 'z') {
+                tempStr += str.charAt(i);
             }
             
-            if(cur >= '0' && cur <= '9') {
-                temp += myId.charAt(i);
+            if(str.charAt(i) >= '0' && str.charAt(i) <= '9') {
+                tempStr += str.charAt(i);
             }
             
-            if(cur == '-' || cur == '_' || cur == '.') {
-                temp += myId.charAt(i);
+            if(str.charAt(i) == '-' ||
+               str.charAt(i) == '_' ||
+               str.charAt(i) == '.') {
+                tempStr += str.charAt(i);
             }
         }
-        myId = temp;
         
+        str = tempStr;
         
-        while(myId.indexOf("..") > -1) {
-            myId = myId.replace("..", ".");
+        while(str.indexOf("..") > -1) {
+            str = str.replace("..", ".");
         }
         
-        if(myId.length() > 0 && myId.charAt(0) == '.') {
-            if(myId.length() > 1) myId = myId.substring(1);
-            else myId = "";
-        } 
-        
-        if(myId.length() > 0 && myId.charAt(myId.length() - 1) == '.') {
-            if(myId.length() - 1 > 0) myId = myId.substring(0, myId.length() - 1);
-            else myId = "";
+        if(str.charAt(0) == '.') {
+            str = str.substring(1);
+        }
+        if(str.length() >= 1 && str.charAt(str.length() - 1) == '.') {
+            str = str.substring(0, str.length() - 1);
         }
         
-        if(myId.length() == 0) myId = "a";
-        
-        if(myId.length() > 15) {
-            myId = myId.substring(0, 15);
-            if(myId.charAt(14) == '.') myId = myId.substring(0, 14);
+        if(str.equals("")) {
+            str += "a";
         }
         
-        while(myId.length() <= 2) {
-            myId += myId.charAt(myId.length() - 1);
+        if(str.length() >= 16) {
+            str = str.substring(0, 15);
+            if(str.charAt(str.length() - 1) == '.') str = str.substring(0, 14);
         }
         
-        return myId;
+        while(str.length() < 3) {
+            str = str + str.substring(str.length() - 1);
+        }
+        
+        return str;
     }
 }
