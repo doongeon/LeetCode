48048cb9943b7a6a018fc01eba7dfcbd2d6a2305
Dong Geon Na <87890694+doongeon@users.noreply.github.com>
2025-06-18 13:09:42 +0900

    [level 2] Title: 후보키, Time: 12.36 ms, Memory: 84.8 MB -BaekjoonHub

---
+++ 프로그래머스/2/42890. 후보키/후보키.java
@@ -0,0 +1,74 @@
+import java.util.*;
+
+class Solution {
+    public int solution(String[][] relation) {
+        int C = relation[0].length;
+        Set<String> v = new HashSet<>();
+        List<Set<Integer>> candidates = new ArrayList<>();
+        Queue<Set<Integer>> q = new ArrayDeque<>();
+        for(int i = 0; i < C; i++) {
+            Set<Integer> temp = new TreeSet<>();
+            temp.add(i);
+            q.offer(temp);
+        }
+        
+        while(!q.isEmpty()) {
+            Set<Integer> colSet = q.poll();    
+            boolean isMinColSet = true;
+        
+            for(Set<Integer> candidate : candidates) {
+                boolean allIncluded = true;
+
+                for(int col : candidate) {
+                    if(!colSet.contains(col)) {
+                        allIncluded = false;
+                        break;
+                    }
+                }
+
+                if(allIncluded) {
+                    isMinColSet = false;
+                    break;
+                }
+            }
+            
+            if(!isMinColSet) continue;
+                
+            
+            Set<String> keySet = new HashSet<>();
+            boolean isUnique = true;
+            for(String[] row : relation) {
+                StringBuilder sb = new StringBuilder();
+                for(int c : colSet) {
+                    sb.append(row[c]);
+                    sb.append("/");
+                }
+                String key = sb.toString();
+                if(keySet.contains(key)) {
+                    isUnique = false;
+                    break;
+                }
+                keySet.add(key);
+            }
+            
+            if(isUnique) {
+                candidates.add(colSet);
+                continue;
+            }
+            
+            
+            for(int c = 0; c < C; c++) {
+                if(colSet.contains(c)) continue;
+                
+                Set<Integer> nextColSet = new TreeSet<>(colSet);
+                nextColSet.add(c);
+                if(v.contains(nextColSet.toString())) continue;
+                
+                v.add(nextColSet.toString());
+                q.offer(nextColSet);
+            }
+        }
+        
+        return candidates.size();
+    }
+}
