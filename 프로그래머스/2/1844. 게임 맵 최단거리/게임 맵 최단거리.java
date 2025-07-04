5f561a34cde6bf6e2ec3ee60204daaaf57ba164f
Dong Geon Na <87890694+doongeon@users.noreply.github.com>
2025-07-04 09:51:00 +0900

    [level 2] Title: 게임 맵 최단거리, Time: 8.89 ms, Memory: 54.9 MB -BaekjoonHub

--- 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.java
+++ 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.java
@@ -2,40 +2,43 @@ import java.util.*;
 
 class Solution {
     public int solution(int[][] maps) {
-        int R = maps.length, C = maps[0].length;
+        final int R = maps.length;
+        final int C = maps[0].length;
+        int answer = 1;
         Queue<int[]> q = new ArrayDeque<>();
-        boolean[][] v = new boolean[R][C];
-        int answer = 0;
-        
-        v[0][0] = true;
         q.offer(new int[] {0, 0});
         
-        int[] dx = {1 ,0, 0, -1};
-        int[] dy = {0, 1, -1, 0};
+        int[] dy = {1, -1, 0, 0};
+        int[] dx = {0, 0, -1, 1};
+        
+        boolean[][] v = new boolean[R][C];
+        v[0][0] = true;
         
         while(!q.isEmpty()) {
-            int qSize = q.size();
+            final int qSize = q.size();
             
-            for(int i = 0 ; i < qSize; i++) {
+            for(int i = 0; i < qSize; i++) {
                 int[] cur = q.poll();
-                if(cur[0] == R - 1 && cur[1] == C - 1) return answer + 1;
+                
+                if(cur[0] == R - 1 && cur[1] == C - 1) return answer;
                 
                 for(int d = 0; d < 4; d++) {
-                    int[] next = {cur[0] + dy[d], cur[1] + dx[d]};
+                    int nextR = cur[0] + dy[d];
+                    int nextC = cur[1] + dx[d];
                     
-                    if(next[0] < 0 || next[0] >= R) continue;
-                    if(next[1] < 0 || next[1] >= C) continue;
-                    if(maps[next[0]][next[1]] == 0) continue;
-                    if(v[next[0]][next[1]]) continue;
+                    if(nextR < 0 || nextR >= R) continue;
+                    if(nextC < 0 || nextC >= C) continue;
+                    if(maps[nextR][nextC] == 0) continue;
+                    if(v[nextR][nextC]) continue;
                     
-                    v[next[0]][next[1]] = true;
-                    q.offer(next);
+                    v[nextR][nextC] = true;
+                    q.offer(new int[] {nextR, nextC});
                 }
             }
             
+            
             answer++;
         }
-        
         
         return -1;
     }
