0f4e36d78e77ff96234ad28fc40aa6a8f9e1e9bd
Dong Geon Na <87890694+doongeon@users.noreply.github.com>
2025-07-04 11:42:19 +0900

    [level 3] Title: 합승 택시 요금, Time: 27.85 ms, Memory: 65.5 MB -BaekjoonHub

--- 프로그래머스/3/72413. 합승 택시 요금/README.md
+++ 프로그래머스/3/72413. 합승 택시 요금/README.md
@@ -1,10 +1,10 @@
 # [level 3] 합승 택시 요금 - 72413 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/72413?language=java) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/72413) 
 
 ### 성능 요약
 
-메모리: 70 MB, 시간: 32.24 ms
+메모리: 65.5 MB, 시간: 27.85 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 07월 03일 00:06:02
+2025년 07월 04일 11:42:16
 
 ### 문제 설명
 
--- 프로그래머스/3/72413. 합승 택시 요금/합승 택시 요금.java
+++ 프로그래머스/3/72413. 합승 택시 요금/합승 택시 요금.java
@@ -2,35 +2,41 @@ import java.util.*;
 
 class Solution {
     public int solution(int n, int s, int a, int b, int[][] fares) {
-        int[][] dist = new int[3][n + 1];
-        int[][] coast = new int[n + 1][n + 1];
-        for(int i = 0; i < 3; i++)
-            Arrays.fill(dist[i], Integer.MAX_VALUE);
-        
-        for(int[] fare : fares) {
-            coast[fare[0]][fare[1]] = fare[2];
-            coast[fare[1]][fare[0]] = fare[2];
+        int answer = 0;
+        List<int[]>[] adj = new ArrayList[n + 1];
+        for(int i = 0; i <= n; i++) {
+            adj[i] = new ArrayList<>();
         }
         
-        final int[] startPoints = {s, a, b};
+        for(int[] fare : fares) {
+            adj[fare[0]].add(new int[] {fare[1], fare[2]});
+            adj[fare[1]].add(new int[] {fare[0], fare[2]});
+        }
+        
+        int[][] dist = new int[3][n + 1];
+        for(int i = 0; i < 3; i++) Arrays.fill(dist[i], Integer.MAX_VALUE);
+        int[] startPoints = {s, a, b};
         for(int i = 0; i < 3; i++) {
-            int start = startPoints[i];
+            int[] d = dist[i];
+            int startPoint = startPoints[i];
             Queue<int[]> pq = new PriorityQueue<>((e1, e2) -> e1[1] - e2[1]);
-            dist[i][start] = 0;
-            pq.offer(new int[] {start, 0});
+            pq.offer(new int[] {startPoint, 0});
+            d[startPoint] = 0;
+            
             while(!pq.isEmpty()) {
                 int[] cur = pq.poll();
-                for(int j = 1; j <= n; j++) {
-                    if(coast[cur[0]][j] == 0) continue;
-                    if(cur[1] + coast[cur[0]][j] < dist[i][j]) {
-                        dist[i][j] = cur[1] + coast[cur[0]][j];
-                        pq.offer(new int[] {j, dist[i][j]});
-                    }
+                
+                if(cur[1] > d[cur[0]]) continue;
+                
+                for(int[] e : adj[cur[0]]) {
+                    if(cur[1] + e[1] >= d[e[0]]) continue;
+                    d[e[0]] = cur[1] + e[1];
+                    pq.offer(new int[] {e[0], d[e[0]]});
                 }
             }
         }
         
-        int answer = Integer.MAX_VALUE;
+        int minCoast = Integer.MAX_VALUE;
         for(int i = 1; i <= n; i++) {
             int sum = 0;
             for(int j = 0; j < 3; j++) {
@@ -40,9 +46,9 @@ class Solution {
                 }
                 sum += dist[j][i];
             }
-            answer = Math.min(answer, sum);
+            minCoast = Math.min(minCoast, sum);
         }
         
-        return answer;
+        return minCoast;
     }
 }
