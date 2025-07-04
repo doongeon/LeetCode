ca77f21bb6f35432f52dcd9284e7f43e711e9302
Dong Geon Na <87890694+doongeon@users.noreply.github.com>
2025-07-04 09:34:34 +0900

    [level 2] Title: 전력망을 둘로 나누기, Time: 0.41 ms, Memory: 75.6 MB -BaekjoonHub

--- 프로그래머스/2/86971. 전력망을 둘로 나누기/전력망을 둘로 나누기.java
+++ 프로그래머스/2/86971. 전력망을 둘로 나누기/전력망을 둘로 나누기.java
@@ -1,42 +1,35 @@
 import java.util.*;
 
 class Solution {
-    int minDiff = 10000000;
-    int N = 0;
+    private int minDiff = Integer.MAX_VALUE;
     
     public int solution(int n, int[][] wires) {
         List<Integer>[] adj = new ArrayList[n + 1];
-        boolean[] v = new boolean[n + 1];
-        N = n;
         
         for(int i = 0; i <= n; i++) {
             adj[i] = new ArrayList<>();
         }
         
         for(int[] wire : wires) {
-            int from = wire[0];
-            int to = wire[1];
-            
-            adj[from].add(to);
-            adj[to].add(from);
+            adj[wire[0]].add(wire[1]);
+            adj[wire[1]].add(wire[0]);
         }
         
-        dfs(1, adj, v);
+        dfs(1, adj, new boolean[n + 1], n);
         
         return minDiff;
     }
     
-    private int dfs(int node, List<Integer>[] adj, boolean[] v) {
-        v[node] = true;
+    public int dfs(int node, List<Integer>[] adj, boolean[] v, int n) {
         int count = 1;
         
         for(int next : adj[node]) {
             if(v[next]) continue;
-            count += dfs(next, adj, v);
+            v[next] = true;
+            count += dfs(next, adj, v, n);
         }
         
-        int tempDiff = Math.abs(N - count * 2);
-        minDiff = Math.min(tempDiff, minDiff);
+        minDiff = Math.min(minDiff, Math.abs(n - count - count));
         
         return count;
     }
