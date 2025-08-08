b9d5e8495bbf8fb3c4560b03167194ce08e0412d
Dong Geon Na <87890694+doongeon@users.noreply.github.com>
2025-08-08 11:30:30 +0900

    [level 2] Title: 주식가격, Time: 24.03 ms, Memory: 75.4 MB -BaekjoonHub

--- 프로그래머스/2/42584. 주식가격/주식가격.java
+++ 프로그래머스/2/42584. 주식가격/주식가격.java
@@ -2,12 +2,15 @@ import java.util.*;
 
 class Solution {
     public int[] solution(int[] prices) {
-        int[] answer = new int[prices.length];
-        Deque<Integer> stack = new ArrayDeque<>();
-        for(int i = 0; i < prices.length; i++) {
+        ArrayDeque<Integer> stack = new ArrayDeque<>();
+        int[] result = new int[prices.length];
+        
+        stack.push(0);
+        
+        for(int i = 1; i < prices.length; i++) {            
             while(!stack.isEmpty() && prices[stack.peek()] > prices[i]) {
                 int idx = stack.pop();
-                answer[idx] = i - idx;
+                result[idx] = i - idx;
             }
             
             stack.push(i);
@@ -15,9 +18,9 @@ class Solution {
         
         while(!stack.isEmpty()) {
             int idx = stack.pop();
-            answer[idx] = prices.length - idx - 1;
+            result[idx] = prices.length - idx - 1;
         }
         
-        return answer;
+        return result;
     }
 }
