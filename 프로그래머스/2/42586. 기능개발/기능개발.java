9f1d5e2eacd2f881cf72fe65ac70841b6f825db2
Dong Geon Na <87890694+doongeon@users.noreply.github.com>
2025-11-15 12:31:18 +0900

    [level 2] Title: 기능개발, Time: 0.71 ms, Memory: 73.4 MB -BaekjoonHub

--- 프로그래머스/2/42586. 기능개발/기능개발.java
+++ 프로그래머스/2/42586. 기능개발/기능개발.java
@@ -2,31 +2,31 @@ import java.util.*;
 
 class Solution {
     public int[] solution(int[] progresses, int[] speeds) {
+        int[] remain_day = new int[progresses.length];
+        for(int i = 0; i < remain_day.length; i++) {
+            remain_day[i] = (int) Math.ceil((100.0 - progresses[i]) / speeds[i]);
+        }
+        
+        System.out.println(Arrays.toString(remain_day));
+        
         Queue<Integer> q = new ArrayDeque<>();
-        for(int i = 0; i < progresses.length; i++) {
-            int t = (int)Math.ceil((100.0 - progresses[i])  / speeds[i]);
-            q.offer(t);
-        }
-        
-        
-        Queue<Integer> tempQ = new ArrayDeque<>();
-        
-        while(!q.isEmpty()) {
-            int base = q.poll();
-            int count = 1;
-            while(!q.isEmpty() && q.peek() <= base) {
+        int max = remain_day[0];
+        int count = 1;
+        for(int i = 1; i < remain_day.length; i++) {
+            if(remain_day[i] <= max) {
                 count++;
-                q.poll();
+            } else {
+                q.offer(count);
+                count = 1;
+                max = remain_day[i];
             }
-            tempQ.offer(count);
         }
+        q.offer(count);
         
-        int[] answer = new int[tempQ.size()];
+        int[] answer = new int[q.size()];
         for(int i = 0; i < answer.length; i++) {
-            answer[i] = tempQ.poll();
+            answer[i] = q.poll();
         }
-        
-        
         return answer;
     }
 }
