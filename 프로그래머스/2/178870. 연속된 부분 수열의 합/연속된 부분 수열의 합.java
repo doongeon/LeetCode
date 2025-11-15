d78ca115f5c4e2e2e055ec979450587e8b4e09e0
Dong Geon Na <87890694+doongeon@users.noreply.github.com>
2025-11-15 14:05:01 +0900

    [level 2] Title: 연속된 부분 수열의 합, Time: 15.20 ms, Memory: 135 MB -BaekjoonHub

--- 프로그래머스/2/178870. 연속된 부분 수열의 합/연속된 부분 수열의 합.java
+++ 프로그래머스/2/178870. 연속된 부분 수열의 합/연속된 부분 수열의 합.java
@@ -2,38 +2,28 @@ import java.util.*;
 
 class Solution {
     public int[] solution(int[] sequence, int k) {
-        int tempSum = 0;
-        int head = 0; int tail = 0;
-        int minLength = 1000000;
-        int[] result = new int[2];
-        
-        for(int i = 0; i < sequence.length; i++) {
-            tail = i;
-            tempSum += sequence[tail];
-            
-            if(tempSum == k) {
-                if(head == tail) return new int[]{head, head};
-                if(tail - head < minLength) {
-                    minLength = tail - head;
-                    result[0] = head;
-                    result[1] = tail;
+        int min_length = Integer.MAX_VALUE;
+        int[] answer = null;
+        int head = 0, tail = 0;
+        int temp_sum = sequence[0];
+        while(head <= tail) {
+            if(temp_sum < k) {
+                if(tail + 1 >= sequence.length) break;
+                tail++;
+                temp_sum += sequence[tail];
+            } else if(temp_sum > k){
+                temp_sum -= sequence[head];
+                head++;
+            } else {
+                int temp_length = tail - head;
+                if(temp_length < min_length) {
+                    min_length = temp_length;
+                    answer = new int[] {head, tail};
                 }
-            }
-            
-            while(tempSum > k) {
-                tempSum -= sequence[head++];
-            }
-            
-            if(tempSum == k) {
-                if(head == tail) return new int[]{head, head};
-                if(tail - head < minLength) {
-                    minLength = tail - head;
-                    result[0] = head;
-                    result[1] = tail;
-                }
+                temp_sum -= sequence[head];
+                head++;
             }
         }
-        
-        return result;
+        return answer;
     }
 }
