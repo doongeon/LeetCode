ed33189cec818081bc0ee169c374263688ced4e0
Dong Geon Na <87890694+doongeon@users.noreply.github.com>
2025-11-15 16:40:31 +0900

    [level 3] Title: 양과 늑대, Time: 13.57 ms, Memory: 80.6 MB -BaekjoonHub

--- 프로그래머스/3/92343. 양과 늑대/양과 늑대.java
+++ 프로그래머스/3/92343. 양과 늑대/양과 늑대.java
@@ -2,48 +2,53 @@ import java.util.*;
 
 class Solution {
     public int solution(int[] info, int[][] edges) {
-        List<List<Integer>> adj = new ArrayList<>();
-        for(int i = 0; i < info.length; i++) adj.add(new ArrayList<>());
-        for(int[] edge : edges) {
-            int from = edge[0];
-            int to = edge[1];
-            
-            adj.get(from).add(to);
-            adj.get(to).add(from);
+        List<Integer>[] adj_list = new ArrayList[info.length];
+        for(int i = 0; i < info.length; i++) adj_list[i] = new ArrayList<>();
+        for(int[] e : edges) {
+            adj_list[e[0]].add(e[1]);
         }
         
-        Set<Integer> next = new HashSet<>();
-        next.add(0);
-        boolean[] v = new boolean[info.length];
-        v[0] = true;
+        Queue<Node> q = new ArrayDeque<>();
+        Set<Integer> s = new HashSet<>();
+        for(int child : adj_list[0]) s.add(child);
+        q.offer(new Node(1, 0, s));
+        int answer = 0;
         
-        return dfs(0, 0, 0, next, adj, info, v);
+        while(!q.isEmpty()) {
+            Node cur = q.poll();
+            answer = Math.max(answer, cur.sheep);
+            
+            for(int next : cur.next) {
+                if(info[next] == 0) {
+                    Set<Integer> temp_next = new HashSet(cur.next);
+                    temp_next.remove(next);
+                    for(int child : adj_list[next]) temp_next.add(child);
+                    Node nextNode = new Node(cur.sheep + 1, cur.wolf, temp_next);
+                    q.offer(nextNode);
+                } else {
+                    if(cur.sheep <= cur.wolf + 1) continue;
+                    
+                    Set<Integer> temp_next = new HashSet(cur.next);
+                    temp_next.remove(next);
+                    for(int child : adj_list[next]) temp_next.add(child);
+                    Node nextNode = new Node(cur.sheep, cur.wolf + 1, temp_next);
+                    q.offer(nextNode);
+                }
+            }
+        }
+        
+        return answer;
     }
     
-    private int dfs(final int node, final int sheep, final int wolf, final Set<Integer> next, final List<List<Integer>> adj, final int[] info, final boolean[] v) {
-        Set<Integer> newNext = new HashSet<>(next);
-        newNext.remove(node);
+    class Node {
+        int sheep;
+        int wolf;
+        Set<Integer> next;
         
-        int newSheep = sheep;
-        int newWolf = wolf;
-        
-        if(info[node] == 0) newSheep++;
-        else newWolf++;
-        
-        if(newWolf >= newSheep) return 0;
-        
-        for(int child : adj.get(node)) {
-            if(v[child]) continue;
-            newNext.add(child);
+        public Node(int sheep, int wolf, Set<Integer> next) {
+            this.sheep = sheep;
+            this.wolf = wolf;
+            this.next = next;
         }
-        
-        int maxSheep = newSheep;
-        for(int n : newNext) {
-            v[n] = true;
-             maxSheep = Math.max(maxSheep, dfs(n, newSheep, newWolf, newNext, adj, info, v));
-            v[n] = false;
-        }
-        
-        return maxSheep;
     }
 }
