f68cc6659084f5ef05ef5d023c50d28b085f6d9d
Dong Geon Na <87890694+doongeon@users.noreply.github.com>
2025-11-15 15:03:17 +0900

    Time: 4 ms (20.63%), Space: 46.6 MB (18.98%) - LeetHub

--- 0529-minesweeper/0529-minesweeper.java
+++ 0529-minesweeper/0529-minesweeper.java
@@ -1,47 +1,59 @@
 class Solution {
-    int[] dx = {1, 1, 1, 0, 0, -1, -1, -1};
-    int[] dy = {1, 0, -1, 1, -1, 1, 0, -1};
-    int R = 0;
-    int C = 0;
-
-
     public char[][] updateBoard(char[][] board, int[] click) {
-        R = board.length;
-        C = board[0].length;
-        int r = click[0], c = click[1];
+        int click_r = click[0], click_c = click[1];
 
-        if(board[r][c] == 'M') {
-            board[r][c] = 'X';
-        } else if(board[r][c] == 'E') {
-            int adjM = 0;
+        if(board[click_r][click_c] == 'M') {
+            board[click_r][click_c] = 'X';
+            return board;
+        }
 
-            for(int d = 0; d < 8; d++) {
-                int adjR = r + dy[d];
-                int adjC = c + dx[d];
+        int num_adj_mines = getNumOfAdjMines(click_r, click_c, board);
+        if(num_adj_mines > 0) {
+            board[click_r][click_c] = (char) ((int) '0' + num_adj_mines);
+            return board;
+        } else {
+            Queue<int[]> q = new ArrayDeque<>();
+            int[] dr = {-1, -1, -1, 0, 0, 1, 1, 1};
+            int[] dc = {-1, 0, 1, -1, 1, -1, 0, 1};
+            board[click_r][click_c] = 'B';
+            q.offer(new int[] {click_r, click_c});
 
-                if(adjR < 0 || adjR >= R) continue;
-                if(adjC < 0 || adjC >= C) continue;
-                if(board[adjR][adjC] == 'M' || board[adjR][adjC] == 'X') adjM++;
-            }
-
-            if(adjM > 0) {
-                // reveal only one;
-                board[r][c] = (char) (adjM + '0');
-            } else {
-                board[r][c] = 'B';
+            while(!q.isEmpty()) {
+                int[] cur = q.poll();
+                
                 for(int d = 0; d < 8; d++) {
-                    int adjR = r + dy[d];
-                    int adjC = c + dx[d];
+                    int[] next = {cur[0] + dr[d], cur[1] + dc[d]};
+                    if(next[0] < 0 || next[0] >= board.length) continue;
+                    if(next[1] < 0 || next[1] >= board[0].length) continue;
+                    if(board[next[0]][next[1]] != 'E') continue;
 
-                    if(adjR < 0 || adjR >= R) continue;
-                    if(adjC < 0 || adjC >= C) continue;
-                    if(board[adjR][adjC] == 'E') {
-                        board = updateBoard(board, new int[]{adjR, adjC});
+                    int temp_num_adj_mines = getNumOfAdjMines(next[0], next[1], board);
+                    if(temp_num_adj_mines > 0) {
+                        board[next[0]][next[1]] = (char) ((int) '0' + temp_num_adj_mines);
+                    } else {
+                        board[next[0]][next[1]] = 'B';
+                        q.offer(next);
                     }
                 }
             }
         }
 
         return board;
+    }
+
+    private int getNumOfAdjMines(int r, int c, char[][] board) {
+        int num_adj_mines = 0;
+        int[] dr = {-1, -1, -1, 0, 0, 1, 1, 1};
+        int[] dc = {-1, 0, 1, -1, 1, -1, 0, 1};
+        for(int d = 0; d < 8; d++) {
+            int adj_r = r + dr[d];
+            int adj_c = c + dc[d];
+
+            if(adj_r < 0 || adj_r >= board.length) continue;
+            if(adj_c < 0 || adj_c >= board[0].length) continue;
+            if(board[adj_r][adj_c] == 'M') num_adj_mines++;
+        }
+
+        return num_adj_mines;
     }
 }
